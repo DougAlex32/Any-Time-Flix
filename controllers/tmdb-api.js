b3aeaad6a6581c335f58b297c386ec16b71593b2
Mike Gustafson <93888281+mike-gustafson@users.noreply.github.com>
2023-12-18 17:24:56 -0800

    feat: tmdb api in place, test made but not ran.  user stories added to resouces

---
+++ controllers/tmdb-api.js
@@ -0,0 +1,121 @@
+const express = require('express');
+const axios = require('axios');
+
+const router = express.Router();
+
+const TMDB_API_KEY = process.env.TMDB_API_KEY;
+const TMDB_BASE_URL = 'https://api.themoviedb.org/3';
+
+// Search for movies
+router.get('/search/:query', async (req, res) => {
+    try {
+        const { query } = req.params;
+        const response = await axios.get(`${TMDB_BASE_URL}/search/movie`, {
+            params: {
+                query,
+                api_key: TMDB_API_KEY,
+            },
+        });
+        res.json(response.data);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+// Get movie details by ID
+router.get('/movie/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const response = await axios.get(`${TMDB_BASE_URL}/movie/${id}`, {
+            params: {
+                api_key: TMDB_API_KEY,
+            },
+        });
+        res.json(response.data);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+// Get movie credits (cast and crew) by ID
+router.get('/movie/:id/credits', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const response = await axios.get(`${TMDB_BASE_URL}/movie/${id}/credits`, {
+            params: {
+                api_key: TMDB_API_KEY,
+            },
+        });
+        res.json(response.data);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+// Discover movies by genre
+router.get('/discover/:genre', async (req, res) => {
+    try {
+        const { genre } = req.params;
+        const response = await axios.get(`${TMDB_BASE_URL}/discover/movie`, {
+            params: {
+                with_genres: genre,
+                api_key: TMDB_API_KEY,
+            },
+        });
+        res.json(response.data);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+// Get movie recommendations by ID
+router.get('/movie/:id/recommendations', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const response = await axios.get(`${TMDB_BASE_URL}/movie/${id}/recommendations`, {
+            params: {
+                api_key: TMDB_API_KEY,
+            },
+        });
+        res.json(response.data);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+// Get popular movies
+router.get('/popular', async (req, res) => {
+    try {
+        const response = await axios.get(`${TMDB_BASE_URL}/movie/popular`, {
+            params: {
+                api_key: TMDB_API_KEY,
+            },
+        });
+        res.json(response.data);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+// Get now playing movies
+router.get('/now-playing', async (req, res) => {
+    try {
+        const response = await axios.get(`${TMDB_BASE_URL}/movie/now_playing`, {
+            params: {
+                api_key: TMDB_API_KEY,
+            },
+        });
+        res.json(response.data);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+module.exports = router;
---
+++ resources/mike's user stories.md
@@ -0,0 +1,17 @@
+As a user, I want to search for movies by title, genre, or actor so that I can find movies I'm interested in watching.
+
+As a user, I want to create a watchlist where I can save movies I intend to watch in the future.
+
+As a user, I want to see detailed information about a movie, including its plot, release date, cast, and crew.
+
+As a user, I want to receive personalized movie recommendations based on my viewing history and preferences.
+
+As a user, I want to create and manage custom playlists or collections of movies so that I can easily access my favorite films.
+
+As a user, I want to rate and review movies I've watched to share my opinions with the community and help others discover good films.
+
+As a user, I want to receive notifications or updates about new releases, upcoming movies, or events related to my favorite actors or genres.
+
+As a user, I want to have the option to connect my social media accounts to share my movie activities with friends or followers.
+
+As a user, I want to have a seamless and user-friendly experience on both desktop and mobile devices to access Any Time Flix from anywhere.
