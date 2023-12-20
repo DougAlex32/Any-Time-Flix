1c401b436ae5c309d41fbd1ecbd4f677b2904bc3
Mike Gustafson <93888281+mike-gustafson@users.noreply.github.com>
2023-12-19 21:26:07 -0800

    feat: connect to DB, add home route

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const react = require('react');
 const express = require('express');
 const methodOverride = require('method-override');
 const mongoose = require('mongoose');
@@ -7,12 +8,25 @@ const cors = require('cors');
 const app = express();
 
 // middleware
+app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(cors());
+app.use(methodOverride('_method'));
+
+// connect to database
+require('dotenv').config();
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => {
+    console.log('Connected to MongoDB');
+}).catch((err) => {
+    console.error('Error connecting to MongoDB:', err);
+});
 
 app.use('/users', require('./controllers/users'));
 app.use('/movies', require('./controllers/tmdb-api'));
+
 app.get('/', (req, res) => {
     return res.json({ message: 'Welcome to AnyTime Flix' });
 });
