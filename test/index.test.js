d38d51b2a0202132770f3da41e852d389a8ba3be
Your Name <you@example.com>
2024-01-06 14:54:58 -0500

    feat: get route for index test passing

--- test/index.test.js
+++ test/index.test.js
@@ -1,6 +1,7 @@
-var expect = require('chai').expect;
-var request = require('supertest');
-var app = require('../server');
+const app = require('../app');
+const expect = require('chai').expect;
+const request = require('supertest');
+
 
 describe('App', function() {
   it('should return a 200 response', function(done) {
